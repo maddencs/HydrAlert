f889a04abb5351e471288623d627314345428e3b
maddencs <maddencs@gmail.com>
2015-05-15 19:12:19 -0700

    .gitignore should work now

--- HydrAlert/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url, patterns
-from django.contrib import admin
-from Hydra import views
-
-
-urlpatterns = patterns('',
-    url(r'^Hydra/', include('Hydra.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    )
--- Hydra/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from Hydra.models import Sensors, Plot, Reservoir
-
-
-admin.site.register(Plot)
-admin.site.register(Reservoir)
-admin.site.register(Sensors)
